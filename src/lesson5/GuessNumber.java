17c922714ef6562a3dad59f8a4e08b50fcf771d0
gurbanovnihad <7404@holbertonstudents.com>
2024-03-01 14:53:40 +0400

    For, while and do while loops

--- src/lesson5/GuessNumber.java
+++ src/lesson5/GuessNumber.java
@@ -1,20 +1,19 @@
 package lesson5;
 
-import java.util.Objects;
 import java.util.Scanner;
 
 public class GuessNumber {
     public static void main(String[] args) {
         boolean playGame = true;
         int computersChoice = (int) (Math.random() * 10);
-        System.out.println("DEBUG: computers choice is " + computersChoice);
+//        System.out.println("DEBUG: computers choice is " + computersChoice);
         Scanner scanner = new Scanner(System.in);
 
         do {
             System.out.println("Welcome to Guess The Number Game!\nGuess a number:");
             String playersGuess = scanner.nextLine();
             // 4. Break statement
-            if (Objects.equals(playersGuess, "exit")) {
+            if (playersGuess.equals("exit")) {
                 System.out.println("Game terminated");
                 break;
             }
