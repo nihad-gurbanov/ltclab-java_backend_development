ba6f6f3b6922a1a22c6a673dc7aa26272f0764d3
gurbanovnihad <7404@holbertonstudents.com>
2024-03-15 11:45:43 +0400

    variable name change

--- src/lesson8/Packer.java
+++ src/lesson8/Packer.java
@@ -11,16 +11,16 @@ public class Packer {
         int bigPackSize = 5; //kgs
         int smallPackSize = 1; //kg
 
-        int requiredBigPack = totalWeight / bigPackSize;
-        int requiredSmallPack = (totalWeight %  bigPackSize) / smallPackSize;
+        int requiredBigPack = totalWeight / bigPackSize; // 12 / 5 = 2
+        int requiredSmallPack = (totalWeight %  bigPackSize) / smallPackSize; // 12 % 5 / 1 = 2
 
-        int totalNeededPack = requiredSmallPack + requiredBigPack;
+        int totalRequiredPack = requiredSmallPack + requiredBigPack;
         int totalGivenPack = bigPack + smallPack;
         boolean success = true;
 
         if (bigPack < 0 || smallPack < 0 || totalWeight < 0){
             return !success;
-        } else if (totalGivenPack < totalNeededPack) {
+        } else if (totalGivenPack < totalRequiredPack) {
             return !success;
         }
         return success;
@@ -29,5 +29,6 @@ public class Packer {
 
 //        return (bigPack < 0 || smallPack < 0 || totalWeight < 0) ? !success :
 //                (totalGivenPack < totalNeededPack) != success;
+
     }
 }
