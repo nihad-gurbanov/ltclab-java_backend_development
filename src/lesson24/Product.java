4d1de0f5e62b30149f92537c6fead2201ec60dcd
gurbanovnihad <7404@holbertonstudents.com>
2024-05-19 18:18:17 +0400

    Supermarket task

---
+++ src/lesson24/Product.java
@@ -0,0 +1,8 @@
+package lesson24;
+
+public class Product {
+    long id;
+    String name;
+    double price;
+    double amount;
+}
---
+++ src/lesson24/Supermarket.java
@@ -0,0 +1,8 @@
+package lesson24;
+
+import java.util.List;
+
+public class Supermarket {
+    List<Product> products;
+
+}
