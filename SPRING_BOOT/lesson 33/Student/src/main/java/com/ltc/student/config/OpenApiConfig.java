dc7b677cc93686e1edb5e462caa366339f594035
Nihad Gurbanov <7404@holbertonstudents.com>
2024-07-19 08:57:10 +0400

    tasks scheduled

--- SPRING_BOOT/lesson 33/Student/src/main/java/com/ltc/student/config/OpenApiConfig.java
+++ SPRING_BOOT/lesson 33/Student/src/main/java/com/ltc/student/config/OpenApiConfig.java
@@ -1,14 +1,34 @@
 package com.ltc.student.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Student Management System",
-                version = "1.0",
-                description = "Student Management System API"
-        )
-)
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Student API")
+                        .version("1.0")
+                        .description("API documentation for the Student application"));
+    }
 }
+
+//package com.ltc.student.config;
+//
+//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+//import io.swagger.v3.oas.annotations.info.Info;
+//
+//@OpenAPIDefinition(
+//        info = @Info(
+//                title = "Student Management System",
+//                version = "1.0",
+//                description = "Student Management System API"
+//        )
+//)
+//public class OpenApiConfig {
+//}
+
--- SPRING_BOOT/lesson 33/Student/src/main/java/com/ltc/student/service/EmailService.java
+++ SPRING_BOOT/lesson 33/Student/src/main/java/com/ltc/student/service/EmailService.java
@@ -4,14 +4,19 @@ import com.ltc.student.dto.request.EmailPictureRequestDto;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @AllArgsConstructor
+@Slf4j
+@Component
 public class EmailService {
     private final JavaMailSender javaMailSender;
 
@@ -42,5 +47,16 @@ public class EmailService {
         }
 
         javaMailSender.send(message);
+    }
+
+
+    @Scheduled(fixedRate = 5000)
+    public void checkIncomingEmails() {
+        log.info("Checking incoming emails");
+    }
+
+    @Scheduled(cron = "0 0 12 ? * MON-FRI")
+    public void sendProgressReport() {
+        log.info("Report sent");
     }
 }
